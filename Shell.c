7c0a897a1c72eacf4a9e1b5a0c02bcd39f48da1a
Kenzon7 <kenzon7g@gmail.com>
2017-03-15 04:57:10 +0200

    Pre-polish(w\o user input)

--- Shell.c
+++ Shell.c
@@ -8,10 +8,12 @@
 
 
 char *array[22];
-char cmd[] = "/bin/ls -i -s > file.txt";
-char *pot1, *pot2;
+char raw[] = "/bin/ls -i -s < file.txt";
+char *cmd, *file, *tok;
+int loops = 0, mode = 0;
+FILE *fp;
+int pipedes[2];
 
-char *c[] = {"/bin/ls","-i","-s"};
 
 //input function 
 
@@ -30,18 +32,37 @@ void inptloop(char **a) {
 	a[loops++] = NULL;
 }
 
+char s[500];
+
 int main() {
 
-	// while (1) {
+	while (1) {
 
-		// for(int i = 0; i < 500; i++) printf("%s", &cmd[i]);
+		if (strstr(raw, ">>") != NULL) { mode = 3; printf(">>\n");}
+		else if (strstr(raw, ">") != NULL) { printf(">\n"); mode = 2;}
+		else if (strstr(raw, "<") != NULL){ mode = 1;  printf("<\n");}
+		else mode = 0;	
 
-		// pot1=strchr(cmd,">> ");
-		pot1 = strstr(cmd, ">>");
-		if (pot1 != NULL) printf("YO");
-  		// while (pot1!=NULL) {
-		// 	  pot1=strchr(pot1+1,'>');
-  		// }
+		cmd = strtok(raw, ">><");
+		file = strtok(NULL, ">><");
+		cmd[strcspn(cmd, "\n")] = 0;
+		// printf("%s:%s\n", cmd, file);
+
+		switch (mode) {
+			case 3:fp = fopen(file, "a"); break;
+			case 2:fp = fopen(file, "w"); break;
+			case 1:fp = fopen(file, "r"); fgets( raw, 1000, fp ); break;
+			default: fp = NULL;
+		}
+
+		printf("%s",raw);
+		tok = strtok(cmd," ");
+		while ( tok != NULL) {
+			array[loops++] = tok;
+			tok = strtok(NULL," ");
+		}
+
+
 
 		//Input loop
 
@@ -49,22 +70,34 @@ int main() {
 
 		//Exit check
 
-		// int ex = strcmp(array[0],"exit");
-		// if (ex == 0)  break;	
+		int ex = strcmp(array[0],"exit");
+		if (ex == 0)  break;	
 		
 		//Exec & fork 
+
+		if (pipe(pipedes) == -1) { perror("pipe"); exit(EXIT_FAILURE);}
 		
 		pid_t frk = fork();
 
-		if (frk == 0) { 
-			// execvp(c[0], c); 
+		if (frk == 0) {
+			if (mode > 1) {
+				dup2 (pipedes[1], STDOUT_FILENO);
+    			close(pipedes[0]);
+    			close(pipedes[1]); 
+			}
+			execvp(array[0], array); 
 			exit(0);
 		}
 
-		wait(NULL);	
-		// printf("%s",array[1]);
-		// for(int i = 0; i <22; i++) {  array[i] = 0; }
+		if( mode > 1) {
+			close(pipedes[1]);
+			read(pipedes[0], s, sizeof(s));
+			fputs(s,fp);
+			fclose(fp);
+		}
 
-		// }
+		wait(NULL);	
+		break;
+	}
 	return 0;
 }
